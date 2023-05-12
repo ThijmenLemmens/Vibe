49ccb98e0adb87bca9b658cf77b020a2db20bcee
thijmen lemmens <1194425@student.roc-nijmegen.nl>
2023-05-12 23:56:08 +0200

    git push

--- vibe/src/util/TextToSpeech.js
+++ vibe/src/util/TextToSpeech.js
@@ -1,35 +1,40 @@
+import Setting from "./Setting";
+
 export default class TextToSpeech {
+
     element;
     speech;
     synth;
 
     constructor(text) {
-        if (typeof window !== 'undefined' && 'SpeechSynthesisUtterance' in window) {
-            this.synth = window.speechSynthesis;
-            this.speech = new SpeechSynthesisUtterance(text);
-  
-            if (!('speechSynthesis' in window)) {
-                alert("Sorry, your browser doesn't support text to speech!");
-                return;
+        if (Setting.sound) {
+            if (typeof window !== 'undefined' && 'SpeechSynthesisUtterance' in window) {
+                this.synth = window.speechSynthesis;
+                this.speech = new SpeechSynthesisUtterance(text);
+    
+                if (!('speechSynthesis' in window)) {
+                    alert("Sorry, your browser doesn't support text to speech!");
+                    return;
+                }
             }
-
-            console.log("Test")
-        }
+        }  
     }
   
     addEvent(string) {
-        if (typeof window !== 'undefined') {
-            this.element = document.getElementById(string);
-  
-            this.element.addEventListener('mouseenter', (e) => {
-                e.preventDefault();
-                this.play();
-            });
+        if (Setting.sound) {
+            if (typeof window !== 'undefined') {
+                this.element = document.getElementById(string);
     
-            this.element.addEventListener('mouseleave', (e) => {
-                e.preventDefault();
-                this.stop();
-            });
+                this.element.addEventListener('mouseenter', (e) => {
+                    e.preventDefault();
+                    this.play();
+                });
+        
+                this.element.addEventListener('mouseleave', (e) => {
+                    e.preventDefault();
+                    this.stop();
+                });
+            }
         }
     }
   
