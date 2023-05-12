30c273e2979a7ad4157ed1cc4ad10e81a7481f39
thijmen lemmens <1194425@student.roc-nijmegen.nl>
2023-05-12 22:22:13 +0200

    Fucking dave grieft!

--- vibe/src/pages/index.js
+++ vibe/src/pages/index.js
@@ -1,11 +1,19 @@
 import { Navbar } from "@/components/navbar/navbar";
 
+import TextToSpeech from "@/util/TextToSpeech"; 
+import dynamic from "next/dynamic";
+
 const Home = () => {
+
+    const tts = new TextToSpeech("Dit is een testcase!");
+
+    tts.addEvent("button");
 
     return (
         <main>
             <Navbar />
             <h1>Home</h1>
+            <button id="button"> Test </button>
         </main>
     )
 }
